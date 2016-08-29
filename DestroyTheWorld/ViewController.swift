bd7da357e1f86a5e2d6ac32881f7385cbd3cd16f
Alejandro Iglesias Peinado <alejandroip@gmail.com>
2016-08-29 10:56:57 +0200

    Initial Commit

---
+++ DestroyTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DestroyTheWorld
+//
+//  Created by Alejandro Iglesias Peinado on 29/8/2016.
+//  Copyright © 2016 Alejandro Iglesias Peinado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
